82788deb327c9b14dc1280b132c407a974302bc9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-08-21 07:02:33 +0000

    Deploy Drne/tank-tactics to github.com/Drne/tank-tactics.git:gh-pages

--- src/utilities/getDistanceBetweenPositions.js
+++
@@ -1,9 +0,0 @@
-export default function getDistanceBetweenPositions(pos1, pos2) {
-    if (!pos1) {
-        return 1000
-    }
-    const distanceBetweenX = Math.abs(pos1[1] - pos2[1])
-    const distanceBetweenY = Math.abs(pos1[0] - pos2[0])
-
-    return Math.max(distanceBetweenX, distanceBetweenY);
-}
